670af86afbde8b0dc07ca939f5fd736ad7297e0f
Opilane TTHK <opilane@tthk.ee>
2025-01-17 15:45:47 +0200

    I added the option to clone

--- 2025contosofuckinyay/Controllers/StudentsController.cs
+++ 2025contosofuckinyay/Controllers/StudentsController.cs
@@ -79,7 +79,7 @@ namespace _2025contosofuckinyay.Controllers
         //create meetod, sisestab andmebaasi uue õpilase. insert new student into database
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,LastName,FirstMidName,EnrollmentDate")] Student student)
+        public async Task<IActionResult> Create([Bind("Id,FirstName,FirstMidName,EnrollmentDate")] Student student)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,24 @@ namespace _2025contosofuckinyay.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(student);
+
         }
+        //Details GET meetod, kuvab ühe õpilase andmed eraldi lehel
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students
+                .FirstOrDefaultAsync();
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        //Delete Code block
 
         public async Task<IActionResult> Delete(int? id)
         {
@@ -110,6 +127,73 @@ namespace _2025contosofuckinyay.Controllers
         {
             var student = await _context.Students.FindAsync(id);
             _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        // Edit Code block
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.Id == id);
+        }
+        //Clone code block
+        public async Task<IActionResult> Clone(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            var ClonedStudent = new Student
+            {
+                FirstName = student.FirstName,
+                FirstMidName = student.FirstMidName,
+                EnrollmentDate = student.EnrollmentDate,
+            };
+            _context.Students.Add(ClonedStudent);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
