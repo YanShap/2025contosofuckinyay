6676c7643fc95e476388562c8ada6780306a3b45
Opilane TTHK <opilane@tthk.ee>
2025-01-17 15:09:55 +0200

    added the option to delete students

--- 2025contosofuckinyay/obj/Debug/net8.0/2025contosofuckinyay.AssemblyInfo.cs
+++ 2025contosofuckinyay/obj/Debug/net8.0/2025contosofuckinyay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2025contosofuckinyay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad9f77ce8d5829d4476abf17ba118f08ea84199")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+265cf99dc655ccd7b5ced2fde1280a5f3e7c56fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("2025contosofuckinyay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2025contosofuckinyay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
